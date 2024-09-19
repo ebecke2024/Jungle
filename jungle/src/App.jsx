1565e8d3c25f341d91c073bae6c10a9c0fdfb8f3
Ammiel <ammiel.maynard@gmail.com>
2024-09-19 14:41:08 -0400

    some changes to navigation bar

--- jungle/src/App.jsx
+++ jungle/src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./components/Home";
+import NavigationBar from "./components/Navbar/NavigationBar";
 
 export default function App() {
   return (
     <div>
+      <NavigationBar />
       <Home />
     </div>
   );
