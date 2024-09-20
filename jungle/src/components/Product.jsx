dd44db5a8bc57afb0c30428d084c3b2d490c3056
Jason Bae <cbae12@umd.edu>
2024-09-20 12:42:15 -0400

    useFetch.jsx and Product.jsx

--- jungle/src/components/Product.jsx
+++ jungle/src/components/Product.jsx
@@ -1,3 +1,30 @@
-export default function Product() {
-    
+import useState from "react";
+import NavigationBar from "./Navbar/NavigationBar";
+
+const ProductList = () => {
+    // const [product, setProduct] = useState(0);
+
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+            <hr/>
+            <h1>All Products</h1>
+            <hr/>
+            <h1>Add Product</h1>
+            <form>
+                Product Name: <input type="text" name="pname"/>
+                <br/>
+                Product Price: <input type="text" name="pprice"/>
+                <br/>
+                Product Stock: <input type="text" name="pstock"/>
+                <br/>
+                Product Availability: <input type="text" name="pavailable"/>
+                <br/>
+                <button type="submit" value="submit">Submit</button>
+                <button type="reset" value="reset">Reset</button>
+            </form>
+        </div>
+    )
 }
+
+export default ProductList;
