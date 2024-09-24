b2cca43475be0541db24172d70d8949bd72c5ce9
Ammiel <ammiel.maynard@gmail.com>
2024-09-24 15:15:53 -0400

    qucik fix

--- jungle/src/components/NotFound.jsx
+++ jungle/src/components/NotFound.jsx
@@ -1,7 +1,8 @@
 export default function NotFound() {
     return (
         <div>
-            <h1>Error 404: page not found</h1>
+            <h1>Error 404: Page not Found</h1>
+            <p>Sorry, this page is not available :(</p>
         </div>
     );
 }
