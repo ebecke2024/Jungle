6f0f5f589b4fdafc3eaf2d5a58d3324effb8e638
Ammiel <ammiel.maynard@gmail.com>
2024-09-25 16:56:53 -0400

    search should work better now

--- jungle/src/App.jsx
+++ jungle/src/App.jsx
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 import { UserProvider } from "./components/UserContext";
+import NavigationBar from "./components/Navbar/NavigationBar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
-import AccountLogin from "./components/AccountLogin"; // Import the login component
-import './Image.css'
+
+import "./Image.css";
+import Account from "./components/Account";
+import Register from "./components/Register";
+import Payment from "./components/Payment";
+import Cart from "./components/Cart";
+import Login from "./components/AccountLogin";
+import NotFound from "./components/NotFound";
+
+export const SearchContext = createContext();
+
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [triggerSearch, setTriggerSearch] = useState(0);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+    {
+      path: "/account",
+      element: <Account />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/payment",
+      element: <Payment />,
+      errorElement: <NotFound />,
+    },
+  ]);
+
   return (
-    <UserProvider>
-      <Home />
-    </UserProvider>
+    <div>
+      <UserProvider>
+        <SearchContext.Provider
+          value={{
+            searchQuery,
+            setSearchQuery,
+            triggerSearch,
+            setTriggerSearch,
+          }}
+        >
+          <NavigationBar />
+          <RouterProvider router={router} />
+        </SearchContext.Provider>
+      </UserProvider>
+    </div>
   );
 }
--- jungle/src/components/Navbar/NavigationBar.jsx
+++ jungle/src/components/Navbar/NavigationBar.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { SearchContext } from "../../App";
 import {
   Navbar,
   NavDropdown,
@@ -10,38 +11,46 @@ import {
 } from "react-bootstrap";
 
 export default function NavigationBar() {
+  const { searchQuery, setSearchQuery, triggerSearch, setTriggerSearch } = useContext(SearchContext);
   // NOTICE: THIS WILL ALL BE REMOVED WHEN WE ACTUALLY IMPLEMENT LOGGING IN
   var accountName = "Placeholder Username";
   var [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const LogoUrl = 'https://i.postimg.cc/pdhx5bzJ/865aa6c1-3d95-4e76-a5a8-d3eb25d3d95e.jpg'
+  const LogoUrl =
+    "https://i.postimg.cc/pdhx5bzJ/865aa6c1-3d95-4e76-a5a8-d3eb25d3d95e.jpg";
   function testLogin() {
     setIsLoggedIn(!isLoggedIn);
+  }
+
+  function handleSearch() {
+    setTriggerSearch(triggerSearch+1);
+    console.log(triggerSearch + " clicked search");
   }
 
   return (
     <Navbar className="bg-body-tertiary justify-content-between">
       <Container>
         <Navbar.Brand href="/">
-        <img
-              src={LogoUrl}
-              width="50"
-              height="50"
-              alt="Logo" />
-        <span className="brand-text">
-            Jungle
-        </span>
+          <img src={LogoUrl} width="50" height="50" alt="Logo" />
+          <span className="brand-text">Jungle</span>
         </Navbar.Brand>
+
         <Form className="d-inline">
           <Row xs={"auto"}>
             <Col>
-              <Form.Control type="text" placeholder="Search Jungle" />
+              <Form.Control
+                type="text"
+                placeholder="Search Jungle"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
             </Col>
             <Col>
-              <Button type="submit">Submit</Button>
+              <Button onClick={handleSearch}>Submit</Button>
             </Col>
           </Row>
         </Form>
+
         <NavDropdown title={isLoggedIn ? accountName : "Not Logged In"}>
           {isLoggedIn ? (
             <div>
@@ -55,7 +64,7 @@ export default function NavigationBar() {
             <div>
               {/* NOTICE: SWITCH TO ROUTE TO LOGIN PAGE LATER*/}
               <NavDropdown.Item href={"/login"}>Login</NavDropdown.Item>
-              <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+              <NavDropdown.Item href={"/register"}>Register</NavDropdown.Item>
             </div>
           )}
         </NavDropdown>
