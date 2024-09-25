6969cb088cfeb775d85f572d4eab30f23be68446
Eric Becker <edbecker1819@gmail.com>
2024-09-25 15:53:45 -0400

    Account Page

--- jungle/src/components/Account.jsx
+++ jungle/src/components/Account.jsx
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
 export default function Account() {
-    return (
-        <div>
-            <h1>This is the Account page</h1>
-        </div>
-    );
+    const [data, setData] = useState([]);
+    const objectAsArray = Object.entries(data);
+  useEffect(() => {
+    // Fetch data from an API
+    fetch('https://localhost:7080/api/Customers/1')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+  console.log(data)
+  return (
+    <div>
+    {objectAsArray.map(([key, value]) => (
+      <p key={key}>{key}: {value}</p>
+    ))}
+  </div>
+);
+  
 }
