3af19b945bf56b0e3ad593e5c0cd41b9afb9602c
Eric Becker <edbecker1819@gmail.com>
2024-09-26 15:46:46 -0400

    Minor error fixed

--- JungleAPI/JungleAPI/obj/Debug/net8.0/JungleAPI.AssemblyInfo.cs
+++ JungleAPI/JungleAPI/obj/Debug/net8.0/JungleAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JungleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35fe78275f92e73b79ea79eb0d855f2d775c78f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e6f3407d7a86f1277ac8fbfca2aabd18e88126")]
 [assembly: System.Reflection.AssemblyProductAttribute("JungleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JungleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
